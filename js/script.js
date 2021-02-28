a9358cde31fdacab10486f84c7cc001d20536c68
wesley oliveira silva <wesoliveirasilva2@gmail.com>
2021-02-28 17:21:31 -0300

    definicao aleatoriedade da frutinha e aumento do tamanho da cobrinha

--- js/script.js
+++ js/script.js
@@ -50,7 +50,7 @@ function iniciarJogo() {
     criarBG();
     criarCobrinha();
     drawFood()
-    
+
     let snakeX = snake[0].x
     let snakeY = snake[0].y
 
@@ -59,7 +59,13 @@ function iniciarJogo() {
     if (direction == 'up') snakeY -= box;
     if (direction == 'down') snakeY += box
 
-    snake.pop();
+    if (snakeX != food.x || snakeY != food.y) {
+        snake.pop();
+    }
+    else {
+        food.x = Math.floor(Math.random() * 15 + 1) * box
+        food.y = Math.floor(Math.random() * 15 + 1) * box
+    }
 
     let newHead = {
         x: snakeX,
